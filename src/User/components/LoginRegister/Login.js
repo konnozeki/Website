5f1d268191b27a3873dbe9556d0f08b82d3702ef
21020424DinhTheVuong <100188715+21020424DinhTheVuong@users.noreply.github.com>
2023-11-13 00:40:48 +0700

    Add Login Register

--- src/User/components/LoginRegister/Login.js
+++ src/User/components/LoginRegister/Login.js
@@ -1,14 +1,129 @@
 //Giao diện login
 //Có thể gộp chung với register
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import { Button, Form, Input, Typography } from 'antd';
+import Nav from '../Nav/Nav';
+import "./Login.scss"
 
+const {Text} = Typography;
 function Login() {
+  window.localStorage.removeItem("isLogedin")
+  window.localStorage.removeItem("isAdmin")
+  window.localStorage.removeItem("isUser")
+  
+  const [username, setUsername] = useState(''); 
+  const [password, setPassword] = useState(''); 
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const loginButtonNavigate = useNavigate();
+  const handleSubmit = () => {
+    if(username ==='' || password === '') {
+    } else {
+      if(username === 'admin' && password !== '') {
+        loginButtonNavigate('/Home')
+        window.localStorage.setItem("isLogedin", true)
+        window.localStorage.setItem("isAdmin", true)
+      } else {
+        loginButtonNavigate("/Home")
+        window.localStorage.setItem("isLogedin", true)
+        window.localStorage.setItem("isUser", true)
+      }
+    }
+  };
+
+  const forgotPasswordNavigate = useNavigate();
+  const handleForgotPassword = () => {
+    forgotPasswordNavigate('/ForgotPassword');
+  }
+  const registerNavigate = useNavigate();
+  const handleRegister = () => {
+    registerNavigate('/Register');
+  }
   return (
     <>
-      <div className="container">
-        <div className='Login'>Login</div>
+      <div className='login-container'>
+      <div className='login'>
+        <div className='title'>LOGIN</div>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
+          >
+          <Input  type="text"
+                  value={username}
+                  onChange={handleUsernameChange}/>
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password type="password"
+                            value={password}
+                            onChange={handlePasswordChange}/>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit" className='button-login' onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Form.Item>
+          
+          <Form.Item>
+            <Text>
+              <p className='forgot-password' onClick={handleForgotPassword}>Forgot Password?</p>
+            </Text>
+            <Text style={{paddingLeft: "10%"}}>
+              New member? <a href="/Register" className='sign-up-now' onClick={handleRegister}>Sign up now</a>
+          </Text>
+          </Form.Item>
+        </Form>
+      </div>    
       </div>
-    </>    )
+    </>
+    
+    )
 }
 
 export default Login
--- src/User/components/LoginRegister/Register.js
+++ src/User/components/LoginRegister/Register.js
@@ -1,14 +1,135 @@
-//Giao diện Đăng ký
-//Có thể gộp chung với login
-import React from 'react'
+//Giao diện login
+//Có thể gộp chung với register
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import { Button, Form, Input, Typography } from 'antd';
+import "./Register.scss"
 
+const {Text} = Typography;
 function Register() {
+
+  const [username, setUsername] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  }
+
+  const registerButtonNavigate = useNavigate();
+  const handleSubmit = () => {
+    
+    if(username === '' || password === '' || confirmPassword === '') {
+    } else {
+      if(username === 'admin' && password !== '' && confirmPassword !== '') {
+        alert("Username mustn't be 'admin' or 'Admin'")
+      } else if (password !== confirmPassword) {
+        alert("Wrong password!")
+      } else {
+        alert("Successful")
+        registerButtonNavigate("/Home")
+        window.localStorage.setItem("isLogedin", true)
+        window.localStorage.setItem("isUser", true)
+      }
+    }
+  };
+
   return (
-    <>
-      <div className="container">
-        <div className='Register'>Register</div>
-      </div>
-    </>    )
+    <div className='register-container'>
+    <div className='register'>
+      <div className='title'>Register</div>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message:  'Please input your username!',
+            },
+          ]}
+          
+        >
+        <Input  type="text"
+                value={username}
+                onChange={handleUsernameChange}/>
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password type="password"
+                          value={password}
+                          onChange={handlePasswordChange}/>
+        </Form.Item>
+
+        <Form.Item
+          label="Confirm"
+          name="confirm-password"
+          rules={[
+            {
+              required: true,
+              message: 'Confirm password!',
+            },
+          ]}
+        >
+          <Input.Password type="password"
+                          value={confirmPassword}
+                          onChange={handleConfirmPassword}/>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" className='button-register' onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form.Item>
+        
+        <Form.Item>
+          <Text style={{paddingLeft: "10%"}}>
+            Have account? <a href="/Login" className='sign-in-now'>Sign in now</a>
+          </Text>
+        </Form.Item>
+      </Form>
+    </div>    
+    </div>
+    
+    )
 }
 
 export default Register
