42d190bf0a4fabc0e8e1766a74ef1d6ce09ddd80
21020544 <114398807+21020544@users.noreply.github.com>
2023-12-23 00:02:30 +0700

    Update Card.js

--- src/User/components/Home/Card.js
+++ src/User/components/Home/Card.js
@@ -1,25 +1,31 @@
+
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 import { Button, Form, Input, Typography } from 'antd'
 import "./Card.scss"
+import Detail from './Detail'
 
-const Card = ({in4phim }) => {
+const Card = ({ in4phim }) => {
     const filmClickNavigate = useNavigate();
     const handleFilmClick = () => {
         filmClickNavigate('/' + {});
     }
+    const [hoveredMovie, setHoveredMovie] = useState(false);
     return (
-        <div className="card">
+        <div className="card" onMouseEnter={() => setHoveredMovie(true)}
+            onMouseLeave={() => setHoveredMovie(false)} >
             <div className="thumbnail-box" onClick={handleFilmClick}>
+                {hoveredMovie &&
+                    (<Detail in4phim={in4phim} />)}
                 <div>
                     <span>
                         <img className='card-image' src={in4phim.Avatar} alt={in4phim.Name}></img>
                     </span>
                 </div>
-                    
-                
+
+
             </div>
-            
+
         </div>
     )
 }
